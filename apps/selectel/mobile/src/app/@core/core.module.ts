98a4012890e8a63e45301590c9bfa81dd7cd7b60
Nikolay Gerzhan <nikolay.gerzhan@gmail.com>
2022-06-06 06:45:11 +0700

    feat(core): add core module

---
+++ apps/selectel/mobile/src/app/@core/core.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BackendService } from './services/backend.services';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+  providers: [BackendService],
+  exports: [],
+})
+export class CoreModule {}
