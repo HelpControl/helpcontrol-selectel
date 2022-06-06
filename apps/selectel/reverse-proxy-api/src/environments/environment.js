064e48517e49b535684499fd4e79e2caf43406c6
Nikolay Gerzhan <nikolay.gerzhan@gmail.com>
2022-06-06 09:00:15 +0700

    feat(reverse-proxy): create reverse proxy

--- apps/selectel/reverse-proxy-api/src/environments/environment.js
+++ apps/selectel/reverse-proxy-api/src/environments/environment.js
@@ -1,3 +1,6 @@
 export const environment = {
   production: false,
+  SERVER_URL: 'https://api.vscale.io',
+  DEV_X_TOKEN:
+    'a16a3c6129c13e4897e7aee7c3a1fc3e5552624be67bce32f98276b7a6c533ad',
 };
